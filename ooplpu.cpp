fb0b20ff2c2c7c9885f9b5a01a7d0b21e6a9f307
Aryanmahakur <mahakuraryan@gmail.com>
2025-08-26 10:40:18 +0530

    uf

--- ooplpu.cpp
+++ ooplpu.cpp
@@ -3,7 +3,22 @@ using namespace std;
 int x=120;
 int main()
 {
-int arr[2]={1,2};
+int arr[5]={1,2,3,4,5};
+int n= sizeof(arr)/sizeof(arr[0]);
+cout<<" enter numbers"<<endl;
+for (int i = 0; i < n; i++)
+{
+   cin>>arr[i];
+  
+}
+
+
+cout<<"numbers you have entered"<<endl;
+for (int i = 0; i < n; i++)
+{
+  cout<<arr[i]<<"  ";
+}
+
     
    return 0;
 }
